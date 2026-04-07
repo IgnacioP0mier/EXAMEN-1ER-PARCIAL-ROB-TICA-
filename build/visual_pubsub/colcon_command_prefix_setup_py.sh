065338c7ecd56f037a48964ff9cfef07b42bdf40
FrMiMoAl <32112849+FrMiMoAl@users.noreply.github.com>
2026-04-06 22:32:02 -0400

    Limpieza: eliminando carpetas de compilación build, install y log

--- build/visual_pubsub/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/franco/Desktop/Robotica/Parcial/install/custom_interface/share/custom_interface/package.sh"
--- build/visual_pubsub/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/franco/Desktop/Robotica/Parcial/install/visual_pubsub'
